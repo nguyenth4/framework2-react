1b1d2d1231a567eab5fdf88aead5a94cef7cfd7e
Trần Hoàng Nguyện <nguyenhoang280004@gmail.com>
2026-03-27 09:57:07 +0700

    Add files via upload

---
+++ myoto-react/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ClientLayout from "./components/layout/ClientLayout";
+import Home from "./pages/Home";
+import CarList from "./pages/CarList";
+import CarDetail from "./pages/CarDetail";
+import Host from "./pages/Host";
+import Register from "./pages/Register";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ClientLayout />}>
+          <Route index element={<Home />} />
+          <Route path="cars" element={<CarList />} />
+          <Route path="cars/:id" element={<CarDetail />} />
+          <Route path="host" element={<Host />} />
+          <Route path="register" element={<Register />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
